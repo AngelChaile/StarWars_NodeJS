111694431b84f1d2de9e8dd1f19d47b36f8b99ab
Angel Chaile <angelchaile90@gmail.com>
2025-03-09 16:42:07 -0300

    mejoras en optimizacion

--- app.js
+++ app.js
@@ -1,28 +1,24 @@
-document.getElementById('anterior').addEventListener('click', async () => {
-    cambiarPagina(-1);
-});
-
-document.getElementById('siguiente').addEventListener('click', async () => {
-    cambiarPagina(1);
+document.addEventListener("DOMContentLoaded", () => {
+    document.getElementById('anterior').addEventListener('click', () => cambiarPagina(-1));
+    document.getElementById('siguiente').addEventListener('click', () => cambiarPagina(1));
+    obtenerPersonajes();
 });
 
 let currentPage = 1;
 
 function cambiarPagina(direccion) {
-    currentPage += direccion;
-    if (currentPage < 1) {
-        currentPage = 1;
+    if (currentPage + direccion > 0) {
+        currentPage += direccion;
+        obtenerPersonajes();
     }
-    obtenerPersonajes();
 }
 
 function mostrarPersonajes(personajes) {
     const personajesList = document.getElementById('personajes-list');
-    // Limpia la lista actual antes de agregar nuevos elementos
     personajesList.innerHTML = '';
 
     // Itera sobre los personajes y crea elementos <li> para mostrarlos
-    personajes.forEach((personaje) => {
+    personajes.forEach(personaje => {
         const listItem = document.createElement('li');
         listItem.textContent = personaje.name;
 
@@ -37,54 +33,44 @@ function mostrarPersonajes(personajes) {
 
         personajesList.appendChild(listItem);
     });
+
     actualizarPaginacion();
 }
 
 function obtenerIdDelPlaneta(url) {
     const matches = url.match(/(\d+)\/$/);
-    if (matches && matches[1]) {
-        return matches[1];
-    }
-    return null;
+    return matches ? matches[1] : null;
 }
 
 async function obtenerInformacionPlaneta(planetId) {
     try {
         const response = await fetch(`/planeta/${planetId}`);
-        if (response.ok) {
-            return await response.json();
-        } else {
-            console.error('Error al obtener información del planeta');
-        }
+        if (!response.ok) throw new Error('Error al obtener información del planeta');
+
+        return await response.json();
     } catch (error) {
         console.error(error);
     }
 }
 
 function mostrarInformacionPlaneta(planeta) {
-    const planetaInfo = document.getElementById('planeta-info');
-    // Actualiza el contenido de la sección con la información del planeta
-    planetaInfo.innerHTML = `<h2>Planeta de origen:</h2><p>${planeta.name}</p>`;
+    document.getElementById('planeta-info').innerHTML = `<h2>Planeta de origen:</h2><p>${planeta.name}</p>`;
 }
 
 function actualizarPaginacion() {
     document.getElementById('page-number').textContent = `Página ${currentPage}`;
-    obtenerPersonajes();
 }
 
 async function obtenerPersonajes() {
-    // Solicitud al backend para obtener personajes paginados
+// Solicitud al backend para obtener personajes paginados
     try {
         const response = await fetch(`/personajes?page=${currentPage}`);
-        if (response.ok) {
-            const data = await response.json();
-            mostrarPersonajes(data);
-        } else {
-            console.error('Error al obtener personajes');
-        }
+        if (!response.ok) throw new Error('Error al obtener personajes');
+
+        const data = await response.json();
+        mostrarPersonajes(data);
+        actualizarPaginacion();
     } catch (error) {
         console.error(error);
     }
 }
-
-obtenerPersonajes();
--- index.js
+++ index.js
@@ -1,43 +1,36 @@
 const express = require('express');
-const aplicacion = express();
 const axios = require('axios');
 const path = require('path');
 
 // Configura Express para servir archivos estáticos desd la misma carpeta que contiene 'index.js'.
-aplicacion.use(express.static(__dirname));
+const app = express();
+app.use(express.json());
+app.use(express.static(path.join(__dirname)));
 
 // Ruta para obtener un listado de personajes
-aplicacion.get('/personajes', async (req, res) => {
-  try {
-    const page = req.query.page || 1; // Página predeterminada: 1
-    const limit = req.query.limit || 10; // Límite predeterminado: 10
+app.get('/personajes', async (req, res) => {
+    try {
+        const page = parseInt(req.query.page) || 1;
+        const limit = parseInt(req.query.limit) || 10;
 
-    const response = await axios.get(`https://swapi.dev/api/people/?page=${page}&limit=${limit}`);
-    const personajes = response.data.results; 
-
-    res.json(personajes);
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ error: 'Error al obtener los personajes' });
-  }
+        const response = await axios.get(`https://swapi.dev/api/people/?page=${page}`);
+        res.json(response.data.results);
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: 'Error al obtener los personajes' });
+    }
 });
 
 // Ruta para obtener información de un planeta en particular
-aplicacion.get('/planeta/:id', async (req, res) => {
-  const planetId = req.params.id;
-
-  try {
-    const response = await axios.get(`https://swapi.dev/api/planets/${planetId}`);
-    const planeta = response.data; 
-    res.json(planeta);
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ error: 'Error al obtener información del planeta' });
-  }
+app.get('/planeta/:id', async (req, res) => {
+    try {
+        const response = await axios.get(`https://swapi.dev/api/planets/${req.params.id}`);
+        res.json(response.data);
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: 'Error al obtener información del planeta' });
+    }
 });
 
-const puerto = 8080; // Puerto disponible
-
-aplicacion.listen(puerto, () => {
-  console.log(`Aplicacion funcionando en el puerto ${puerto}`);
-});
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => console.log(`Servidor en http://localhost:${PORT}`));
